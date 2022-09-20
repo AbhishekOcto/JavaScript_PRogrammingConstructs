fefe2203b9e9286852c518829115ff61ecf25392
Abhishek <you@example.com>
2022-09-20 10:01:03 +0530

    [Abhishek]Added: Sequence practice problems

---
+++ SequenceProblems/RandomInDice.js
@@ -0,0 +1,4 @@
+//Use REPL – Use Random to get Dice Number between 1 to 6
+let random;
+random = random = Math.floor(Math.random() * 6) +1;
+console.log("Random number in a dice: " +random);
---
+++ SequenceProblems/UseRandom.js
@@ -0,0 +1,4 @@
+//to get Single Digit using random
+let random;
+random = Math.floor(Math.random() * 10);
+console.log("The Random Number is: ",random);
